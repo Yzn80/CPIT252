69ebc9e9ddcc8985b612dd278d29128603c8f115
Yzn80 <91143859+Yzn80@users.noreply.github.com>
2022-10-18 13:49:44 +0300

    Delete CPIT252 - Lab 5/lab-5-main directory

--- CPIT252 - Lab 5/lab-5-main/src/main/java/edu/kau/fcit/cpit252/WeatherCity.java
+++
@@ -1,4 +0,0 @@
-package edu.kau.fcit.cpit252;
-public interface WeatherCity {
-    public String getWeatherInfo(String city);
-}
--- CPIT252 - Lab 5/lab-5-main/src/main/java/edu/kau/fcit/cpit252/WeatherGeo.java
+++
@@ -1,5 +0,0 @@
-package edu.kau.fcit.cpit252;
-
-public interface WeatherGeo {
-    public String getWeatherInfo(double latitude, double longitude);
-}
